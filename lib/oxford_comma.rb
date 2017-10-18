17d5a76470311a097fb2a612003e480007f65fef
Monica  Karasek <mlkarasek@gmail.com>
2017-10-18 23:46:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,11 @@
 def oxford_comma(array)
-  if array.size == 2
-    array.join(" and ")
-  elsif 2 < array.size
-    last_word = array.pop
+if (array.count == 1)
+  return array.join
+elsif (array.count == 2)
+  return array.join(" and ")
+elsif (array.count >= 3 )
+  return array[0..-2].join(", ") + ", and " + array[-1]
+else
+  return array
+end
 end
