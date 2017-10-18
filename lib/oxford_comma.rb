b560e39f3c292dc14c090f9b94d2cf4ac7f1d70b
Monica  Karasek <mlkarasek@gmail.com>
2017-10-18 23:46:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+if (array.count == 1)
+  return array.join
+elsif (array.count == 2)
+  return array.join(" and ")
+elsif (array.count >= 3 )
+  return array[0..-2].join(", ") + ", and " + array[-1]
+else
+  return array
+end
 end
